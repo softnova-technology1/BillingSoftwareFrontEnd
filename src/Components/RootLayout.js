a9c9acc0adcc3c36b398c87881ff7526da6c2e96
alagammaiL <alagulakshmanan10398@gmail.com>
2025-08-08 11:23:57 +0530

    initialCommit8-8-25

---
+++ src/Components/RootLayout.js
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+import Button from "@mui/material/Button";
+export default function RootLayout() {
+  return (
+    <>
+      <Button>dfdsf</Button>
+      <Outlet />
+    </>
+  );
+}
